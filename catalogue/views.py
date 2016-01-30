da55187f968092fda030f3fe018c77f563723b6c
Arian Bakh <arianbakh@gmail.com>
2016-01-30 11:23:38 +0330

    Resolved bugs

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import redis
 
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
@@ -11,6 +12,9 @@ from catalogue.models import *
 from catalogue.forms import *
 
 
+REDIS_CLIENT = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
 @login_required
 def home(request, category='all'):
     # if category == 'all':
@@ -20,14 +24,17 @@ def home(request, category='all'):
     # context = {'request': request, 'tasks': tasks, 'categories':Category.objects.all(), 'selected_category':category}
     # return render_to_response("tasks.html", context)
 
+    REDIS_CLIENT.incr('home', 1)
+
     top_search_queries = SearchQuery.objects.values('text').annotate(Count('text')).order_by('-text__count')[:5]
     most_posted_employers = MyUser.objects.annotate(Count('task_employer')).filter(task_employer__count__gt=0).order_by('-task_employer__count')[:5]
     employees = MyUser.objects.annotate(Count('task_employee')).filter(task_employee__count__gt=0)
-    best_employees = employees.annotate(Avg('comment_employee__rate'))
+    best_employees = employees.annotate(Avg('comment_employee__rate')).order_by('-comment_employee__rate__avg')[:5]
     context = {
         'top_search_queries': top_search_queries,
         'most_posted_employers': most_posted_employers,
         'best_employees': best_employees,
+        'total_home_visits': REDIS_CLIENT.get('home'),
     }
     return render_to_response("home.html", context, context_instance=RequestContext(request))
 
