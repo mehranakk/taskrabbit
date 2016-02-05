e65760b5ac4a7c92dabec1440f5febd292e3cf29
Arian Bakh <arianbakh@gmail.com>
2016-02-05 21:42:33 +0330

    New task page UI polish

--- catalogue/forms.py
+++ catalogue/forms.py
@@ -1,19 +1,22 @@
 from django import forms
 from .models import MyUser, Task, Skill
 
+
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = MyUser
         fields = ['display_name', 'skills']
-       # widgets = {
-       #         'skills': forms.ChoiceField(choices=Skill.objects.all())
-       #     }
+        # widgets = {
+        #     'skills': forms.ChoiceField(choices=Skill.objects.all())
+        # }
+
 
 class NewProfileForm(forms.Form):
     username = forms.CharField(label="Username", required=True)
     password = forms.CharField(label="Password", widget=forms.PasswordInput, required=True)
     email = forms.EmailField(label="Email", required=True)
-    display_name = forms.CharField(label="Displaye Name", required=True)
+    display_name = forms.CharField(label="Display Name", required=True)
+
 
 class NewTaskForm(forms.ModelForm):
     class Meta:
